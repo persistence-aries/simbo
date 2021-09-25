30bbc180589e09bcebfa257fcd584f6dd419a700
Aries Chang <44836584+persistence-aries@users.noreply.github.com>
2021-09-25 12:19:41 +0800

    Update simbo_stack.java

--- simbo_stack.java
+++ simbo_stack.java
@@ -1,1 +1,36 @@
 
+import java.util.Stack;
+
+public class simbo_stack {
+	Stack<Integer> stack = new Stack();
+	Stack<Integer> maxStack = new Stack();
+	
+	
+	
+    public void push(int x) {
+        if(maxStack.isEmpty() || x >= maxStack.pop()) {
+        	maxStack.push(x);
+        }
+    	stack.push(x);
+    }
+    
+    public void pop() {
+        if(stack.pop().equals(maxStack.pop())){
+        	maxStack.pop();
+        } 
+        stack.pop();
+    }
+    
+    
+    public int top() {
+        return stack.pop();
+    }
+
+    public int getMax() {
+        return maxStack.pop();
+    }
+    
+    
+
+}
+
